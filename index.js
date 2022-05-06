417a0bd90e2b9a9adcec339a9dc2604c743fa6df
tomikyblog <104985843+tomikyblog@users.noreply.github.com>
2022-05-06 23:03:58 +0900

    It can be done with images other than webp.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const sharp = require("sharp");
 const fs = require("fs")
 const files = fs.readdirSync('./in')
+const allow_exts = new Array('jpg', 'jpeg', 'png', 'webp')
 
 function webpToJpeg () {
   return new Promise((resolve, reject) => {
     for (let index = 0; index < files.length; index++) {
-      const file = files[index];
-      if (file.indexOf('webp') != -1) {
+      const file = files[index]
+      const position = file.lastIndexOf('.')
+      const ext = file.slice(position + 1)
+      ext.toLowerCase()
+
+      if (allow_exts.indexOf(ext) !== -1) {
         const inpath = `./in/${file}`
         const outpath = `./out/jpeg/${file.split('.')[0]}.jpeg`
         sharp(inpath).toFile(outpath)
